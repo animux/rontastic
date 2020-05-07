53dd62f3467ac65421edbff00775d02a15e42819
Dev4m1 <djami1562@gmail.com>
2020-05-07 22:45:19 +0600

    Heroku Deployment (all done)

--- server.js
+++ server.js
@@ -8,4 +8,20 @@ const App = Express();
 App.use(Express.static('./dist'));
 App.use('/project', Express.static('./dist/img/projects'));
 
+App.get('/instagram', (request, response) => {
+	response.redirect('https://instagram.com/rontastic_im');
+	return response.end();
+});
+
+App.get('/facebook', (request, response) => {
+	response.redirect('https://facebook.com/im.rontastic');
+	return response.end();
+});
+
+App.get('/artstation', (request, response) => {
+	response.redirect('https://www.artstation.com/rontasticim');
+	return response.end();
+});
+
+
 App.listen(PORT, () => console.log('Listening on port 3000'));
